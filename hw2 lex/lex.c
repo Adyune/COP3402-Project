ca943e1fe7cadf42813cee31d232646a029b509d
Adam <62030290+Adyune@users.noreply.github.com>
2021-10-15 16:37:28 -0400

    Added lex analyzer files
    Wrote code to scan for numbers

---
+++ hw2 lex/lex.c
@@ -0,0 +1,211 @@
+/* 
+	This is the lex.c file for the UCF Fall 2021 Systems Software Project.
+	For HW2, you must implement the function lexeme *lexanalyzer(char *input).
+	You may add as many constants, global variables, and support functions
+	as you desire.
+	
+	If you choose to alter the printing functions or delete list or lex_index, 
+	you MUST make a note of that in you readme file, otherwise you will lose 
+	5 points.
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "compiler.h"
+#define MAX_NUMBER_TOKENS 500
+#define MAX_IDENT_LEN 11
+#define MAX_NUMBER_LEN 5
+
+lexeme *list;
+int lex_index;
+
+void printlexerror(int type);
+void printtokens();
+
+
+lexeme *lexanalyzer(char *input)
+{
+	list = malloc(sizeof(lexeme) * MAX_NUMBER_TOKENS);
+	int i = 0, j = 0, counter = 0;
+	char * string = malloc(sizeof(char) * MAX_IDENT_LEN + 1);
+	lexeme token;
+	// TODO: implement var, restricted, and operators/symbols
+	while(input[i] != '\0'){
+		if (iscntrl(input[i])){
+			i++;
+			continue;
+		}
+		// If the character starts with a number 
+		if(isdigit(input[i])){
+			while(input[i] != '\0'){
+				// Invalid Identifier
+				if (isletter(input[i]))
+					printlexerror(2);
+				// Excessive number length
+				else if (j > MAX_NUMBER_LEN)
+					printlexerror(3);
+				// Implement helper function so this doesnt look gross?
+				else if (iscntrl(input[i]) || !isdigit(input[i]))
+				{
+					token.value = atoi(string);
+					token.type = numbersym;
+					i++;
+					j = 0;
+				} else {
+					string[j] = input[i];
+					i++, j++;
+				}
+			}	
+		}
+	}
+	free(string);
+	printtokens();
+	return list;
+}
+
+
+void printtokens()
+{
+	int i;
+	printf("Lexeme Table:\n");
+	printf("lexeme\t\ttoken type\n");
+	for (i = 0; i < lex_index; i++)
+	{
+		switch (list[i].type)
+		{
+			case oddsym:
+				printf("%11s\t%d", "odd", oddsym);
+				break;
+			case eqlsym:
+				printf("%11s\t%d", "==", eqlsym);
+				break;
+			case neqsym:
+				printf("%11s\t%d", "!=", neqsym);
+				break;
+			case lsssym:
+				printf("%11s\t%d", "<", lsssym);
+				break;
+			case leqsym:
+				printf("%11s\t%d", "<=", leqsym);
+				break;
+			case gtrsym:
+				printf("%11s\t%d", ">", gtrsym);
+				break;
+			case geqsym:
+				printf("%11s\t%d", ">=", geqsym);
+				break;
+			case modsym:
+				printf("%11s\t%d", "%", modsym);
+				break;
+			case multsym:
+				printf("%11s\t%d", "*", multsym);
+				break;
+			case divsym:
+				printf("%11s\t%d", "/", divsym);
+				break;
+			case addsym:
+				printf("%11s\t%d", "+", addsym);
+				break;
+			case subsym:
+				printf("%11s\t%d", "-", subsym);
+				break;
+			case lparensym:
+				printf("%11s\t%d", "(", lparensym);
+				break;
+			case rparensym:
+				printf("%11s\t%d", ")", rparensym);
+				break;
+			case commasym:
+				printf("%11s\t%d", ",", commasym);
+				break;
+			case periodsym:
+				printf("%11s\t%d", ".", periodsym);
+				break;
+			case semicolonsym:
+				printf("%11s\t%d", ";", semicolonsym);
+				break;
+			case assignsym:
+				printf("%11s\t%d", ":=", assignsym);
+				break;
+			case beginsym:
+				printf("%11s\t%d", "begin", beginsym);
+				break;
+			case endsym:
+				printf("%11s\t%d", "end", endsym);
+				break;
+			case ifsym:
+				printf("%11s\t%d", "if", ifsym);
+				break;
+			case thensym:
+				printf("%11s\t%d", "then", thensym);
+				break;
+			case elsesym:
+				printf("%11s\t%d", "else", elsesym);
+				break;
+			case whilesym:
+				printf("%11s\t%d", "while", whilesym);
+				break;
+			case dosym:
+				printf("%11s\t%d", "do", dosym);
+				break;
+			case callsym:
+				printf("%11s\t%d", "call", callsym);
+				break;
+			case writesym:
+				printf("%11s\t%d", "write", writesym);
+				break;
+			case readsym:
+				printf("%11s\t%d", "read", readsym);
+				break;
+			case constsym:
+				printf("%11s\t%d", "const", constsym);
+				break;
+			case varsym:
+				printf("%11s\t%d", "var", varsym);
+				break;
+			case procsym:
+				printf("%11s\t%d", "procedure", procsym);
+				break;
+			case identsym:
+				printf("%11s\t%d", list[i].name, identsym);
+				break;
+			case numbersym:
+				printf("%11d\t%d", list[i].value, numbersym);
+				break;
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("Token List:\n");
+	for (i = 0; i < lex_index; i++)
+	{
+		if (list[i].type == numbersym)
+			printf("%d %d ", numbersym, list[i].value);
+		else if (list[i].type == identsym)
+			printf("%d %s ", identsym, list[i].name);
+		else
+			printf("%d ", list[i].type);
+	}
+	printf("\n");
+	list[lex_index++].type = -1;
+}
+
+void printlexerror(int type)
+{
+	if (type == 1)
+		printf("Lexical Analyzer Error: Invalid Symbol\n");
+	else if (type == 2)
+		printf("Lexical Analyzer Error: Invalid Identifier\n");
+	else if (type == 3)
+		printf("Lexical Analyzer Error: Excessive Number Length\n");
+	else if (type == 4)
+		printf("Lexical Analyzer Error: Excessive Identifier Length\n");
+	else
+		printf("Implementation Error: Unrecognized Error Type\n");
+	
+	free(list);
+	return;
+}
